cf6457b82e0fe20aedcaa0fdfdbaec996f7c42e9
Ankush262003 <asse262003@gmail.com>
2024-05-06 00:14:35 +0530

    added common dialog , text, stack

---
+++ client/src/components/common/Dialog.jsx
@@ -0,0 +1,80 @@
+import React, { memo } from "react";
+import { Dialog as MDialog, DialogTitle, IconButton, Paper, DialogContent, DialogActions } from "@mui/material";
+import { Close, Margin } from "@mui/icons-material";
+import {makeStyles, createStyles} from "@mui/styles";
+import Stack from "./Stack";
+
+const Dialog = (props) =>{
+    const  {
+        title, 
+        onOpen, onClose,
+        contentComponent, actionComponent,
+        disableEnforceFocus = true,
+        children, 
+        sx,
+        ...resetProps
+    } = props
+
+    const classes = useStyles()
+    return(<>
+        <MDialog
+         open={true}
+         clone={false}
+         disableEnforceFocus={""}
+         fullWidth
+         maxWidth="md"
+         aria-labelledby=""
+        //  {...resetProps}
+        >
+            <Stack sx={{
+               padding:"10px 10px",
+               ...sx 
+            }}>
+                <DialogTitle className={classes.DialogTitle}
+                sx={{
+                    padding:"0px"
+                    }}
+                    >
+                    <div>
+                    Dialog Title
+                    </div>
+                    <IconButton>
+                        <Close/>
+                    </IconButton>
+                </DialogTitle>
+                <DialogContent sx={{
+                    padding:"0px"
+                }}>
+                    {contentComponent && contentComponent()}
+                </DialogContent>
+                <DialogActions>
+
+                </DialogActions>
+            </Stack>
+        </MDialog>     
+    </>)
+    
+}
+
+
+const useStyles = makeStyles((theme) =>
+   createStyles({
+    // root:{
+    //     padding:"0px",
+    // },
+    // Paper:{
+    //     Margin:15,
+    //     width:"100%",
+    // },
+
+    // DialogTitle style css:
+    DialogTitle: {
+        display:"flex", justifyContent:"space-between"
+    }
+
+
+
+   })
+)
+
+export default React.memo(Dialog)
--- client/src/components/login/LoginDialog.jsx
+++ client/src/components/login/LoginDialog.jsx
@@ -1,11 +1,51 @@
+import { AppBar, Box, Toolbar, styled } from "@mui/material";
 import React from "react";
+import Stack from "../common/Stack";
+import Dialog from "../common/Dialog";
+import Text from "../common/Text";
 
 const LoginDialog = () =>{
    return(
-    <>
-       <h1>login page and LoginDialog</h1>
-    </>
+    <Box sx={{
+      backgroundColor:"#d1d7db"
+      }}>
+         <AppBar>
+            <StyledToolbar>
+              ,sdjfsj
+            </StyledToolbar>
+         </AppBar>
+         <Dialog
+            contentComponent = {()=>{
+               return <Stack>
+                  <Text>Use WhatsApp on your computer</Text>
+                  <Box>
+                     <ol>
+                        <li>Open WhatsApp on your phone</li>
+                        <li>B</li>
+                        <li>Tap <strong>Linked devices</strong>  and then <strong>Link a device</strong> </li>
+                        <li>Point your phone at this screen to capture the QR code</li>
+                     </ol>
+                  </Box>
+               </Stack>
+            }}           
+         />
+         
+         
+      </Box>
    )
 }
 
-export default LoginDialog;
+
+const StyledToolbar = styled(Toolbar)(({ theme }) => ({
+   alignItems: 'flex-start',
+   paddingTop: theme.spacing(1),
+   paddingBottom: theme.spacing(2),
+   backgroundColor:"#00a884",
+   // Override media queries injected by theme.mixins.toolbar
+   '@media all': {
+     minHeight: "193px",
+   },
+}));
+
+
+export default LoginDialog
